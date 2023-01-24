402fb9808a9dc532d774d55372b3fee54abf2c24
urieisenberg <urieisenberg@gmail.com>
2023-01-24 16:53:09 +0200

    Campaign constructor

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,7 +1,11 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
-    address public manager; 
-    uint public minimumContribution; 
-    
+    address public manager;
+    uint256 public minimumContribution;
+
+    function Campaign(uint256 minimum) public {
+        manager = msg.sender;
+        minimumContribution = minimum;
+    }
 }
