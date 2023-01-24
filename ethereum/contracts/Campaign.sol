2ba0166ce71e9733b9903275103ec3ac9effaed3
urieisenberg <urieisenberg@gmail.com>
2023-01-24 16:54:11 +0200

    approvers variable as an array of addresses

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.17;
 contract Campaign {
     address public manager;
     uint256 public minimumContribution;
+    address[] public approvers;
 
     function Campaign(uint256 minimum) public {
         manager = msg.sender;
