b9389bbcf3ad55d7df2f3860e8b39c2165c37f4e
urieisenberg <urieisenberg@gmail.com>
2023-01-25 11:06:10 +0200

    txt

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -2,37 +2,37 @@ pragma solidity ^0.4.17;
 
 contract Campaign {
     struct Request {
-        string description;
-        uint256 value;
-        address recipient;
-        bool complete;
-        uint approvalCount;
-        mapping(address => bool) approvals;
-    } // struct is a custom data type that can be used to group together related variables of different data types
+        string description; // purpose of the request
+        uint256 value; // amount of ether to transfer
+        address recipient; // who gets the money
+        bool complete; // whether the request has been completed or not
+        uint256 approvalCount; // tracks the number of people who voted for the request
+        mapping(address => bool) approvals; // track number of approvals for each request       
+    } 
 
     Request[] public requests; // array of requests that will be created by the manager
-    address public manager;
-    uint256 public minimumContribution;
-    mapping(address => bool) public approvers;
+    address public manager; // the address of the person who is managing the campaign
+    uint256 public minimumContribution; // the minimum amount of ether that a person has to contribute to become an approver
+    mapping(address => bool) public approvers; // list of addresses for every person who has donated to the campaign
 
     modifier restricted() {
-        require(msg.sender == manager); // make sure the person who is trying to create a request is the manager
-        _; // the underscore is a placeholder for the function that is going to be called}
-    }
+        require(msg.sender == manager); 
+        _; 
+    } // modifier that makes sure only the manager can create a request 
 
     function Campaign(uint256 minimum) public {
         manager = msg.sender;
         minimumContribution = minimum;
-    }
+    } //  constructor function that sets the minimumContribution and the owner
 
     function contribute() public payable {
         require(msg.value > minimumContribution); // make sure the contribution is greater than the minimum contribution
 
         approvers[msg.sender] = true; // add the address of the person who contributed to the approvers mapping
-    }
+    } // called when someone wants to donate money to the campaign and become an approver
 
     function createRequest(
-        string description,
+        string description, 
         uint256 value,
         address recipient
     ) public restricted {
@@ -41,8 +41,8 @@ contract Campaign {
             value: value,
             recipient: recipient,
             complete: false
-        }); // create a new request with the data passed in the function
+        }); 
 
         requests.push(newRequest);
-    }
+    } // called by the manager to create a new spending request
 }
