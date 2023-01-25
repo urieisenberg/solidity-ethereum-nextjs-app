871d2ef3906ec4e412b76a7dbb965e0b045900d0
urieisenberg <urieisenberg@gmail.com>
2023-01-25 12:37:56 +0200

    fix

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.4.17;
 contract CampaignFactory {
     address[] public deployedCampaigns; // array of addresses of all deployed campaigns
 
-    function createCampaign(uint minimum) public {
-      address newCampaign = new Campaign(minimu, msg.sender);
-      deployedCampaigns.push(newCampaign);
+    function createCampaign(uint256 minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
     } // deploys a new instance of a campaign and stores the resulting address
 
-    function getDeployedCampaigns() public view returns (address[]){
+    function getDeployedCampaigns() public view returns (address[]) {
         return deployedCampaigns;
     } // returns a list of all deployed campaigns
 }
@@ -20,21 +20,21 @@ contract Campaign {
         address recipient; // who gets the money
         bool complete; // whether the request has been completed or not
         uint256 approvalCount; // tracks the number of people who voted for the request
-        mapping(address => bool) approvals; // track number of approvals for each request       
-    } 
+        mapping(address => bool) approvals; // track number of approvals for each request
+    }
 
     Request[] public requests; // array of requests that will be created by the manager
     address public manager; // the address of the person who is managing the campaign
     uint256 public minimumContribution; // the minimum amount of ether that a person has to contribute to become an approver
     mapping(address => bool) public approvers; // list of addresses for every person who has donated to the campaign
-    uint public approversCount; // number of people who have donated to the campaign    
+    uint256 public approversCount; // number of people who have donated to the campaign
 
     modifier restricted() {
-        require(msg.sender == manager); 
-        _; 
-    } // modifier that makes sure only the manager can create a request 
+        require(msg.sender == manager);
+        _;
+    } // modifier that makes sure only the manager can create a request
 
-    function Campaign(uint minimum, address creator ) public {
+    function Campaign(uint256 minimum, address creator) public {
         manager = creator;
         minimumContribution = minimum;
     } //  constructor function that sets the minimumContribution and the owner
@@ -47,9 +47,9 @@ contract Campaign {
     } // called when someone wants to donate money to the campaign and become an approver
 
     function createRequest(
-        string description, 
+        string description,
         uint256 value,
-        address recipient,
+        address recipient
     ) public restricted {
         Request memory newRequest = Request({
             description: description,
@@ -57,26 +57,28 @@ contract Campaign {
             recipient: recipient,
             complete: false,
             approvalCount: 0
-        }); 
+        });
 
         requests.push(newRequest);
     } // called by the manager to create a new spending request
 
-    function approveRequest(uint index) public {
-        Request storage request = requests[index]; // get the request from the requests array   
-        require(approvers[msg.sender]); // make sure the person who is approving the request is an approver
-        require(!request[index].approvals[msg.sender]); // make sure the person has not already voted for the request
+    function approveRequest(uint256 index) public {
+        Request storage request = requests[index]; // get the request from the requests array
 
-        request[index].approvals[msg.sender] = true; // add the address of the person who voted to the approvals mapping 
-        request[index].approvalCount++; // increment the approvalCount
+        require(approvers[msg.sender]); // make sure the person who is approving the request is an approver
+        require(!request.approvals[msg.sender]); // make sure the person has not already voted for the request
+
+        request.approvals[msg.sender] = true; // add the address of the person who voted to the approvals mapping
+        request.approvalCount++; // increment the approvalCount
     } // called by each contributor to approve a spending request
 
-    function finalizeRequest(uint index) public restricted {
-        Request storage request = requests[index]; // get the request from the requests array   
-        require(request.approvalCount > (approversCount / 2)); // make sure the request has more than 50% of the approvers voting for it    
-        require(request.complete); // make sure the request has not already been completed 
-        
-        request.recipient.transfer(request.value); // transfer the money to the recipient   
-        request.complete = true; // mark the request as complete 
-    }
+    function finalizeRequest(uint256 index) public restricted {
+        Request storage request = requests[index]; // get the request from the requests array
+
+        require(request.approvalCount > (approversCount / 2)); // make sure the request has more than 50% of the approvers voting for it
+        require(!request.complete); // make sure the request has not already been completed
+
+        request.recipient.transfer(request.value); // transfer the money to the recipient
+        request.complete = true; // mark the request as complete
+    } // after a request has gotten enough approvals, the manager can call this to get money sent to the vendor
 }
