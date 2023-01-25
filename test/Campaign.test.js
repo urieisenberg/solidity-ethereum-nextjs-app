b3a2871b9d266c8e50d110ef76fce5a9a1b6aaa7
urieisenberg <urieisenberg@gmail.com>
2023-01-25 12:41:22 +0200

    test to checks if the minimum contribution is met

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -53,4 +53,17 @@ describe('Campaigns', () => {
     const isContributor = await campaign.methods.approvers(accounts[1]).call();
     assert(isContributor);
   });
+
+  it('requires a minimum contribution', async () => {
+    // checks if the minimum contribution is met
+    try {
+      await campaign.methods.contribute().send({
+        value: '5',
+        from: accounts[1],
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
 });
