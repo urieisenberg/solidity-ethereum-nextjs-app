0802c1f55a61cf827dba42e8ebad1833a2ccbf83
urieisenberg <urieisenberg@gmail.com>
2023-01-25 12:09:18 +0200

    writes the compiled contract to the build folder

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -11,3 +11,9 @@ const output = solc.compile(source, 1).contracts;
 
 fs.ensureDirSync(buildPath); // creates build folder if it doesn't exist
 
+for (let contract in output) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract + '.json'),
+    output[contract]
+  );
+} // writes the compiled contract to the build folder
