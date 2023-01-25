745aeb4e4b73a61c8eb06e2d562d0a00ac052acd
urieisenberg <urieisenberg@gmail.com>
2023-01-25 11:07:30 +0200

    approvalCount for createRequest

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -34,13 +34,14 @@ contract Campaign {
     function createRequest(
         string description, 
         uint256 value,
-        address recipient
+        address recipient,
     ) public restricted {
         Request memory newRequest = Request({
             description: description,
             value: value,
             recipient: recipient,
-            complete: false
+            complete: false,
+            approvalCount: 0
         }); 
 
         requests.push(newRequest);
