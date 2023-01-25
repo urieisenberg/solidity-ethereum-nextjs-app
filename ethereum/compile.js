a031f6c4b68db3d4d8e226c256cc263834fe10a0
urieisenberg <urieisenberg@gmail.com>
2023-01-25 12:06:46 +0200

    write output to the build directory

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -8,3 +8,6 @@ fs.removeSync(buildPath);
 const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
 const source = fs.readFileSync(campaignPath, 'utf8');
 const output = solc.compile(source, 1).contracts;
+
+fs.ensureDirSync(buildPath); // creates build folder if it doesn't exist
+
