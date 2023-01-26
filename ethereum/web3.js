6a822f82d30c93a31d16c60898891588d9db7c2a
urieisenberg <urieisenberg@gmail.com>
2023-01-26 15:39:58 +0200

    fixed 'window is undefiend'

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,7 +1,13 @@
-import Web3 from "web3";
- 
-window.ethereum.request({ method: "eth_requestAccounts" });
- 
-const web3 = new Web3(window.ethereum);
- 
+import Web3 from 'web3';
+
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.ethereum !== undefined) {
+  window.ethereum.request({ method: 'eth_requestAccounts' });
+  web3 = new Web3(window.ethereum);
+} else {
+  const provider = new Web3.providers.HttpProvider(process.env.INFURA);
+  web3 = new Web3(provider);
+}
+
 export default web3;
