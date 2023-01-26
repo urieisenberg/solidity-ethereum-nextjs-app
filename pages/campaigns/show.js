b29ea29da130b843c28bbfe4923b662e4cf594fd
urieisenberg <urieisenberg@gmail.com>
2023-01-26 19:54:20 +0200

    getInitialProps

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Layout } from '../../components/Layout';
+import Layout from '../../components/Layout';
+import Campaign from '../../ethereum/campaign';
 
-const ShowCampaign = () => {
+const ShowCampaign = ({ campaign }) => {
+  const getSummary = async () => {
+    const summary = await campaign.methods.getSummary().call();
+    console.log(summary);
+  };
+
   return (
     <Layout>
       <h3>Show Campaign</h3>
     </Layout>
   );
+};
+
+ShowCampaign.getInitialProps = async (props) => {
+  const campaign = Campaign(props.query.address);
+  return { campaign };
 };
 
 export default ShowCampaign;
