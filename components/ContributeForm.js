0cd15042502a79827d0f8e69a329ba5a50cca951
urieisenberg <urieisenberg@gmail.com>
2023-01-26 20:20:41 +0200

    onSubmit to make a contribution

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
 import { Form, Button, Message, Input } from 'semantic-ui-react';
 import Campaign from '../../ethereum/campaign';
+import web3 from '../../ethereum/web3';
 
-const ContributeForm = ({address}) => {
+const ContributeForm = ({ address }) => {
   const [value, setValue] = useState('');
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
 
     const campaign = Campaign(address);
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods.contribute().send({
+        from: accounts[0],
+        value: web3.utils.toWei(value, 'ether'),
+      });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
