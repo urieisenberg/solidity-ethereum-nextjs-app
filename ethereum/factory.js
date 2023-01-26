a9c0a513050d63f2961e39b7caa4bee143a74337
urieisenberg <urieisenberg@gmail.com>
2023-01-26 17:12:57 +0200

    new address from redeploying

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,10 +1,9 @@
 import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
 
-
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0xa4A297D6E6F42400FD70d9E1BDf4a9030507791C"
+  '0xE50711463Cd834c782CE11d9235f771151Ed8c45'
 );
 
 export default instance;
