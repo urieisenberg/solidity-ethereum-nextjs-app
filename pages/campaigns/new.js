bd58b450556b3d3b621b99d81b6821b1df67c881
urieisenberg <urieisenberg@gmail.com>
2023-01-26 16:18:22 +0200

    create a campaign form

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 
 const NewCampaign = () => {
-  return <Layout>New Campaign</Layout>;
+  const [minimumContribution, setMinimumContribution] = useState('');
+
+  return (
+    <Layout>
+      <h3>Create a Campaign</h3>
+      <Form>
+        <Form.Field>
+          <label>Minimum Contribution</label>
+          <Input
+            label="wei"
+            labelPosition="right"
+            value={minimumContribution}
+            onChange={(event) => setMinimumContribution(event.target.value)}
+          />
+        </Form.Field>
+        <Button primary>Create!</Button>
+      </Form>
+    </Layout>
+  );
 };
 
 export default NewCampaign;
