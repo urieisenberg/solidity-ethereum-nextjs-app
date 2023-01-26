2d1539d96a70bc4c3333e102351a87979922a847
urieisenberg <urieisenberg@gmail.com>
2023-01-26 16:37:41 +0200

    pushRoute after submit

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -3,6 +3,7 @@ import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
+import { Router } from '../../routes';
 
 const NewCampaign = () => {
   const [minimumContribution, setMinimumContribution] = useState('');
@@ -19,6 +20,8 @@ const NewCampaign = () => {
       await factory.methods.createCampaign(minimumContribution).send({
         from: accounts[0],
       });
+
+      Router.pushRoute('/');
     } catch (err) {
       setErrorMessage(err.message);
     }
@@ -29,7 +32,7 @@ const NewCampaign = () => {
   return (
     <Layout>
       <h3>Create a Campaign</h3>
-      <Form onSubmit={onSubmit} error={errorMessage}>
+      <Form onSubmit={onSubmit} error={!!errorMessage}>
         <Form.Field>
           <label>Minimum Contribution</label>
           <Input
