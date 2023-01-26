088db40c64d4c332362e30758d397182bfadb15e
urieisenberg <urieisenberg@gmail.com>
2023-01-26 19:49:02 +0200

    addresses

--- ethereum/const.js
+++ ethereum/const.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: '../.env' });
 
 const MNEMONIC = process.env.MNEMONIC;
 const INFURA = process.env.INFURA;
-const ADDRESS = process.env.ADDRESS;
+const ACCOUNT_ADDRESS = process.env.ACCOUNT_ADDRESS;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 
-module.exports = { MNEMONIC, INFURA, ADDRESS };
+module.exports = { MNEMONIC, INFURA, ACCOUNT_ADDRESS, CONTRACT_ADDRESS };
