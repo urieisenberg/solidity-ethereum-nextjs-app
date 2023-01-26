81bebd0660f454444f1a372250680a4fd3d47b81
urieisenberg <urieisenberg@gmail.com>
2023-01-26 20:09:58 +0200

    state

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -3,15 +3,21 @@ import { Form, Button, Message, Input } from 'semantic-ui-react';
 
 const ContributeForm = () => {
   const [value, setValue] = useState('');
+
   const onSubmit = (event) => {
     event.preventDefault();
   };
 
   return (
-    <Form>
+    <Form onSubmit={onSubmit}>
       <Form.Field>
         <label>Amount to Contribute</label>
-        <Input label="ether" labelPosition="right" />
+        <Input
+          label="ether"
+          labelPosition="right"
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+        />
       </Form.Field>
       <Button primary>Contribute!</Button>
     </Form>
