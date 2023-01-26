b1f6afd8e697c43471251cf514ec1ceb5889772f
urieisenberg <urieisenberg@gmail.com>
2023-01-26 20:02:16 +0200

    render cards

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Card } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
 
@@ -9,9 +10,37 @@ const ShowCampaign = ({
   approversCount,
   manager,
 }) => {
+  const items = [
+    {
+      header: manager,
+      meta: 'Address of Manager',
+      description:
+        'The manager created this campaign and can create requests to withdraw money',
+      style: { overflowWrap: 'break-word' },
+    },
+    {
+      header: minimumContribution,
+      meta: 'Minimum Contribution (wei)',
+      description:
+        'You must contribute at least this much wei to become an approver',
+    },
+    {
+      header: requestsCount,
+      meta: 'Number of Requests',
+      description:
+        'A request tries to withdraw money from the contract. Requests must be approved by approvers',
+    },
+    {
+      header: approversCount,
+      meta: 'Number of Approvers',
+      description: 'Number of people who have already donated to this campaign',
+    },
+  ];
+
   return (
     <Layout>
       <h3>Show Campaign</h3>
+      <Card.Group items={items} />
     </Layout>
   );
 };
