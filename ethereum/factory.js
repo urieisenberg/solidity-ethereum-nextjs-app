69d6714448e6858d7800a6ecc87cbb7b788ea7e8
urieisenberg <urieisenberg@gmail.com>
2023-01-26 15:49:59 +0200

    in

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,11 +1,10 @@
 import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
 
-const ADDRESS = process.env.ADDRESS;
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  ADDRESS
+  "0xa4A297D6E6F42400FD70d9E1BDf4a9030507791C"
 );
 
 export default instance;
