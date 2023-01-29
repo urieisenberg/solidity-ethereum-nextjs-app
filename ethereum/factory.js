ef60f612528b182887d5c91a156a70114eb8b315
urieisenberg <urieisenberg@gmail.com>
2023-01-29 12:34:27 +0200

    CampaignFactory.abi

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -2,7 +2,7 @@ import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
-  JSON.parse(CampaignFactory.interface),
+  CampaignFactory.abi,
   '0xE50711463Cd834c782CE11d9235f771151Ed8c45'
 );
 
