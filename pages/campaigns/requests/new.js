87bd5d1a7a8c1fde033f40e2ae9887689e43c934
urieisenberg <urieisenberg@gmail.com>
2023-01-29 11:15:29 +0200

    getInitialProps

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Form, Button, Message, Input } from 'semantic-ui-react';
+import Layout from '../../../components/Layout';
+import Campaign from '../../../ethereum/campaign';
+import web3 from '../../../ethereum/web3';
+import { Router, Link } from '../../../routes';
 
 const NewRequest = () => {
   return <div>new</div>;
+};
+
+NewRequest.getInitialProps = async (props) => {
+  const { address } = props.query;
+  return { address };
 };
 
 export default NewRequest;
