09723081b54f8c4e0c9efd3f4cbe03ab1fec6015
urieisenberg <urieisenberg@gmail.com>
2023-01-29 11:17:46 +0200

    request creation form

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button, Message, Input } from 'semantic-ui-react';
-import Layout from '../../../components/Layout';
 import Campaign from '../../../ethereum/campaign';
 import web3 from '../../../ethereum/web3';
 import { Router, Link } from '../../../routes';
+import Layout from '../../../components/Layout';
 
 const NewRequest = () => {
-  return <div>new</div>;
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+  const [recipient, setRecipient] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Layout>
+      <h3>Create a Request</h3>
+      <Form onSubmit={onSubmit}>
+        <Form.Field>
+          <label>Description</label>
+          <Input
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Value in Ether</label>
+          <Input value={value} onChange={(e) => setValue(e.target.value)} />
+        </Form.Field>
+        <Form.Field>
+          <label>Recipient</label>
+          <Input
+            value={recipient}
+            onChange={(e) => setRecipient(e.target.value)}
+          />
+        </Form.Field>
+        <Button primary>Create!</Button>
+      </Form>
+    </Layout>
+  );
 };
 
 NewRequest.getInitialProps = async (props) => {
