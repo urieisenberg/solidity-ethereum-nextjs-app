86a227f0a075c552e7b7d559fa9c69fbed92930b
urieisenberg <urieisenberg@gmail.com>
2023-01-29 11:29:39 +0200

    return an array of requests n requestCount props

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Button } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
+import Campaign from '../../../ethereum/campaign';
 
-const RequestIndex = ({ address }) => {
+const RequestIndex = ({ address, requestCount, requests }) => {
   return (
     <Layout>
       <h3>Requests</h3>
@@ -16,7 +17,18 @@ const RequestIndex = ({ address }) => {
 
 RequestIndex.getInitialProps = async (props) => {
   const { address } = props.query;
-  return { address };
+  const campaign = Campaign(address);
+  const requestCount = await campaign.methods.getRequestsCount().call();
+
+  const requests = await Promise.all(
+    Array(requestCount)
+      .fill()
+      .map((element, index) => {
+        return campaign.methods.requests(index).call();
+      })
+  ); // for each request, call the campaign contract and get the request, then return an array of requests
+
+  return { address, requests, requestCount };
 };
 
 export default RequestIndex;
