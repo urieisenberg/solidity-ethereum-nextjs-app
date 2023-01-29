af1125cfb8735372c094c770e2de62886bd7810a
urieisenberg <urieisenberg@gmail.com>
2023-01-29 11:11:11 +0200

    layout

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Layout from '../../../components/Layout';
 
 const RequestIndex = () => {
-  return <div>RequestIndex</div>;
+  return <Layout>RequestIndex</Layout>;
 };
 
 export default RequestIndex;
