d9b0f1bb0d4ac9148756ebf1b389dd7b1715bc11
urieisenberg <urieisenberg@gmail.com>
2023-01-29 12:33:14 +0200

    compiledFactory.evm.bytecode.object

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -13,7 +13,7 @@ const deploy = async () => {
   console.log('Attempting to deploy from account', accounts[0]);
 
   const result = await new web3.eth.Contract(compiledFactory.abi)
-    .deploy({ data: compiledFactory.bytecode })
+    .deploy({ data: compiledFactory.evm.bytecode.object })
     .send({ gas: '1000000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
