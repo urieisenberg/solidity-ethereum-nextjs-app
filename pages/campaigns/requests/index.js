20297a1dd106f358464baf7fc2deeb1594f98eb1
urieisenberg <urieisenberg@gmail.com>
2023-01-29 11:13:00 +0200

    getInitialProps address

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
+import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
 
-const RequestIndex = () => {
-  return <Layout>RequestIndex</Layout>;
+const RequestIndex = ({ address }) => {
+  return (
+    <Layout>
+      <h3>Requests</h3>
+      <Link route={`/campaigns/${address}/requests/new`}>
+        <Button primary>Add Request</Button>
+      </Link>
+    </Layout>
+  );
+};
+
+RequestIndex.getInitialProps = async (props) => {
+  const { address } = props.query;
+  return { address };
 };
 
 export default RequestIndex;
